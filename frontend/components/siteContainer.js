98e4103a37720b838a478402f2ccd563ce87ab3c
James Seppi <james.seppi@gsa.gov>
2017-07-21 13:57:35 -0500

    show error message when site not found for id in params

--- frontend/components/siteContainer.js
+++ frontend/components/siteContainer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import SideNav from './site/SideNav/sideNav';
 import PagesHeader from './site/pagesHeader';
 import AlertBanner from './alertBanner';
+import LoadingIndicator from './loadingIndicator';
 
 const propTypes = {
   params: PropTypes.shape({
@@ -25,7 +26,7 @@ const propTypes = {
   }),
   location: PropTypes.shape({
     pathname: PropTypes.string,
-  }),
+  }).isRequired,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
@@ -34,7 +35,6 @@ const propTypes = {
 
 const defaultProps = {
   children: null,
-  location: null,
   storeState: null,
 };
 
@@ -72,7 +72,24 @@ class SiteContainer extends React.Component {
     const pageTitle = this.getPageTitle(location.pathname);
 
     if (!site) {
-      return null;
+      if (storeState.sites.isLoading) {
+        return <LoadingIndicator />;
+      }
+
+      return (
+        <div className="usa-grid">
+          <div className="usa-alert usa-alert-error" role="alert">
+            <div className="usa-alert-body">
+              <h3 className="usa-alert-heading">Unauthorized</h3>
+              <p className="usa-alert-text">
+                Apologies; you don&apos;t have access to this site in Federalist!
+                <br />
+                Please contact the site owner if you should have access.
+              </p>
+            </div>
+          </div>
+        </div>
+      );
     }
 
     return (
